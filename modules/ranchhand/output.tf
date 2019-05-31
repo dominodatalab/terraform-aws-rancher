5678de038ba02a2be3977424056411048afccc7c
Bernhard (Sonny) Köhler <sonnygarcia@icloud.com>
2019-05-31 08:50:36 -0400

    Add admin password change (#7)
    * adds admin_password variable and support
    
    leveraging new feature in ranchhand.
    
    also fixes an issue where a local copy of the script would be written to
    root when working_dir is not provided.
    
    * propagates admin_password out of inner module
    
    for consuming by other tooling

--- modules/ranchhand/output.tf
+++ modules/ranchhand/output.tf
@@ -1,3 +1,8 @@
 output "cluster_provisioned" {
   value = "${null_resource.provisioner.id}"
 }
+
+output "admin_password" {
+  description = "Generated password for Rancher default admin user"
+  value       = "${random_string.password.result}"
+}
