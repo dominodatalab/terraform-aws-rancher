72a91194db7608659972588a4040a7790239b2cf
Joaquin Lopez <mrgus@disco-zombie.net>
2019-09-23 20:34:00 -0700

    Ordering clearly isn't my thing

--- main.tf
+++ main.tf
@@ -343,7 +343,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand/tree/master/terraform?ref=DOM-14267"
+  source = "github.com/dominodatalab/ranchhand?ref=DOM-14267/terraform"
 
   node_ips = split(
     ",",
