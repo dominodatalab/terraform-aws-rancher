5fd884c0df9643f290898659d27898f2a62d1eac
Joaquin Lopez <mrgus@disco-zombie.net>
2019-10-01 12:52:07 -0700

    Typo

--- main.tf
+++ main.tf
@@ -345,7 +345,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand.git//terraform?ref=v0.1.2-rc1""
+  source = "github.com/dominodatalab/ranchhand.git//terraform?ref=v0.1.2-rc1"
 
   node_ips = split(
     ",",
