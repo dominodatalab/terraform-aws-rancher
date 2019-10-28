2fd0687666297fad78aaf95ed197ae4139c2a978
gsvijayraajaa <vijay.raajaa@dominodatalab.com>
2019-10-28 10:39:48 +0530

    Point to ranchhand release with 2.3.0 update

--- main.tf
+++ main.tf
@@ -347,7 +347,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand?ref=v0.1.3-rc1"
+  source = "github.com/dominodatalab/ranchhand?ref=v0.1.4-rc1"
 
   // If for some bizarre reason you mix public/private hosts, this won't work
   node_ips = formatlist(
@@ -369,4 +369,3 @@ module "ranchhand" {
 
   admin_password = var.admin_password
 }
-
