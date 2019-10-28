508c2e41a9b84400c86e065001bc4a314f3d3716
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-28 16:59:51 -0700

    Upgrade SSH Fix

--- main.tf
+++ main.tf
@@ -347,7 +347,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand?ref=v0.1.3-rc1"
+  source = "github.com/dominodatalab/ranchhand?ref=v0.2.0"
 
   // If for some bizarre reason you mix public/private hosts, this won't work
   node_ips = formatlist(
