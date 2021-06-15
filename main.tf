5b795cf1c26b98b6a3ce992c66dff04e760d6552
Michael Fraenkel <michael.fraenkel@dominodatalab.com>
2021-06-15 07:45:46 -0600

    PLAT-2391: Add retries for helm repos

--- main.tf
+++ main.tf
@@ -359,7 +359,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand?ref=v0.7.1"
+  source = "github.com/dominodatalab/ranchhand?ref=v0.7.2"
 
   node_ips = aws_instance.this.*.private_ip
 
