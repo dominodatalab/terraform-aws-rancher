2f5f5ca1fc317f7009c484c260884b6148fcb26a
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2022-03-08 12:42:54 -0800

    Reference newer ranchhand

--- main.tf
+++ main.tf
@@ -359,7 +359,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand?ref=v0.7.3"
+  source = "github.com/dominodatalab/ranchhand?ref=steved/rancher-update"
 
   node_ips = aws_instance.this.*.private_ip
 
