d1aef4d808556898069b1c49c3f593c3c3dba841
ddl-retornam <56278673+ddl-retornam@users.noreply.github.com>
2023-08-15 22:54:32 -0700

    testing

--- main.tf
+++ main.tf
@@ -359,7 +359,7 @@ resource "aws_security_group_rule" "provisioner_secgrp_ingress_443" {
 # Provisioner
 #------------------------------------------------------------------------------
 module "ranchhand" {
-  source = "github.com/dominodatalab/ranchhand?ref=retornam-v0.8.1"
+  source = "github.com/dominodatalab/ranchhand?ref=retornamupdates"
 
   node_ips = aws_instance.this.*.private_ip
 
